d22ed37cd34a2153042bde35733fdfb523e50d97
Emmanuel Umeh <emmachinemerem04@gmail.com>
2024-05-23 12:01:34 -0700

    sort-middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,13 +14,35 @@
 // }
 
 // Temporarily disable middleware
-export default function ({ store, redirect, route }) {
-  console.log('Disabled middleware');
-  return true;
-}
+// export default function ({ store, redirect, route }) {
+//   console.log('Disabled middleware');
+//   return true;
+// }
 
 // export default function ({ store, redirect }) {
 //   if (store.state.auth.loggedIn) {
 //     return redirect('/')
 //   }
 // }
+
+
+
+
+// middleware/auth.js
+export default function ({ store, redirect, route }) {
+  // Check if the user is trying to access the blog page and is not logged in
+  if (route.path === '/blog' &&!store.state.auth.loggedIn) {
+    // Redirect to the login page
+    return redirect('/login');
+  }
+
+  // If the user is just registered, reset the flag and redirect to the blog page
+  if (store.state.isJustRegistered) {
+    store.commit('resetIsJustRegistered');
+    return redirect('/blog');
+  }
+
+  // Allow access to other routes
+  return true;
+}
+
